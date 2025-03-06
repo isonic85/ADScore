6c536a66764574d80a345e177f9716b0648d2ade
isonic85 <122450848+isonic85@users.noreply.github.com>
2025-03-06 15:40:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,6 @@ function renderGame() {
     document.getElementById("player2-name").textContent = players[1].name;
     document.getElementById("player2-score").textContent = players[1].score;
 
-    document.getElementById("player1").classList.toggle("active", currentPlayer === 0);
-    document.getElementById("player2").classList.toggle("active", currentPlayer === 1);
-
     document.getElementById("throw1").textContent = throws[0] || "-";
     document.getElementById("throw2").textContent = throws[1] || "-";
     document.getElementById("throw3").textContent = throws[2] || "-";
